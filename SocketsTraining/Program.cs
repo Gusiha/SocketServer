3c17a3c5951bbaaca5ba00e499e08c121abc7dd9
Gusiha <TUGARIN@TUGARIN>
2023-11-18 11:42:58 +0300

    Add project files.

---
+++ SocketsTraining/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+int port = 9005;
+IPAddress IPAddress = IPAddress.Parse("192.168.0.102");
+IPEndPoint endPoint = new IPEndPoint(IPAddress, port);
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(endPoint);
+socket.Connect(endPoint);
+socket.Listen();
+
+Console.WriteLine(socket.Available);
+
+socket.Shutdown(SocketShutdown.Both);
+socket.Close();
+
+
