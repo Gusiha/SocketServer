2ee25e41f1769f49d84ba35f13d74f47a7bbf9a3
Gusiha <TUGARIN@TUGARIN>
2023-11-18 12:20:33 +0300

    feat : add receiving info

--- SocketsTraining/Program.cs
+++ SocketsTraining/Program.cs
@@ -1,17 +1,30 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.Unicode;
 
-int port = 9005;
-IPAddress IPAddress = IPAddress.Parse("192.168.0.102");
+byte[] bytes = new byte[256];
+
+int port = 3245;
+IPAddress IPAddress = IPAddress.Parse("192.168.0.105");
 IPEndPoint endPoint = new IPEndPoint(IPAddress, port);
 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 socket.Bind(endPoint);
-socket.Connect(endPoint);
 socket.Listen();
+socket.Accept();
+socket.Receive(bytes);
 
-Console.WriteLine(socket.Available);
 
-socket.Shutdown(SocketShutdown.Both);
-socket.Close();
+while (!bytes.Any())
+{
+    //Console.WriteLine(socket.IsBound);
+}
+
+Console.WriteLine(Encoding.UTF8.GetString(bytes));
+
+//Console.WriteLine(socket.Available);
+
+//socket.Shutdown(SocketShutdown.Both);
+//socket.Close();
 
 
