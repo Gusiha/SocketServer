c11d56d1790b25169fb5c065d6973c8606235331
Gusiha(NB) <tima.milushev@mail.ru>
2023-11-18 20:29:11 +0300

    from lessons

--- SocketsTraining/Program.cs
+++ SocketsTraining/Program.cs
@@ -1,23 +1,45 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Unicode;
 
-byte[] bytes = new byte[256];
 
-int port = 3245;
-IPAddress IPAddress = IPAddress.Parse("192.168.0.105");
+string ipAddress = "10.102.3.185";
+int port = 11000;
+IPAddress IPAddress = IPAddress.Parse(ipAddress);
 IPEndPoint endPoint = new IPEndPoint(IPAddress, port);
 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 socket.Bind(endPoint);
-socket.Listen();
-socket.Accept().Receive(bytes);
-//socket.Receive(bytes);
+
+while (true)
+{
+    byte[] bytes = new byte[1024];
+    string tempString = "";
+    socket.Listen();
+    Socket currentSocket = socket.Accept();
+
+    Console.WriteLine("Connected to " + currentSocket.RemoteEndPoint);
+
+    currentSocket.Receive(bytes);
+    Console.WriteLine($"Message recieved from {currentSocket.RemoteEndPoint} : {Encoding.UTF8.GetString(bytes)}");
+
+    tempString = Encoding.UTF8.GetString(bytes);
+
+
+    var message = Environment.MachineName + " | " + tempString.ToUpper();
+    bytes = Encoding.UTF8.GetBytes(message);
+    Console.WriteLine("Data has been formatted : " + tempString.ToUpper());
+
+    currentSocket.Send(bytes);
+    Console.WriteLine("Data has been sent");
+    currentSocket.Close();
+    Console.WriteLine("Socket is closed");
+}
 
 
 
 
-Console.WriteLine(Encoding.UTF8.GetString(bytes));
 
 //Console.WriteLine(socket.Available);
 
